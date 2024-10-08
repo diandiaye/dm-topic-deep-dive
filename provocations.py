76ef15e683770747e4c97872034f0295f540cd6b
diandiaye <41585144+diandiaye@users.noreply.github.com>
2024-10-08 13:11:50 +0200

    Update provocations.py

--- provocations.py
+++ provocations.py
@@ -76,6 +76,7 @@ def generate_provocations(topic_description, topic_keywords):
     with open(file_path, 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
     print("Script execution completed.")
+    print(json_data)
     return json_data
 
     
